8ff45e8c866e82c8c4fb75793ef3ea9bf09a62be
Lav Kushwaha <lavkumar1224@gmail.com>
2023-11-03 00:22:41 +0530

    Responsive design

---
+++ src/Components/Achivementsection/Achievement.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import './Achievement.css'
+import img1 from '../../assets/asset43.svg'
+import img2 from '../../assets/asset28.svg'
+import img3 from '../../assets/asset29.svg'
+import img4 from '../../assets/asset30.svg'
+
+const Achivement = () => {
+  return (
+     <>
+     <section className='achievements-section'>
+            <div className="achievement-content">
+                <h2>OUR ACHIEVEMENTS</h2>
+                <p><span>ProminentPixel</span> has developed its popularity through its unbeatable services like any other top company. Working on thousands of projects, our expert team has gained the experience in the way that they can manage multiple projects at a time.</p>
+                <p>Since the beginning, we have achieved many awards, appreciation, and most importantly the customer satisfaction.</p>
+            </div>
+            <div className="achievement-number-container">
+                <div className="lines-of-code">
+                    <img src={img1} alt="linesofcode" />
+                    <div className="lines-number">
+                        <h2>3416K</h2>
+                        <p>LINES OF CODE</p>
+                    </div>
+                </div>
+                <div className="lines-of-code">
+                    <img src={img2} alt="linesofcode" />
+                    <div className="lines-number">
+                        <h2>4579</h2>
+                        <p>CUPS OF COFFEE</p>
+                    </div>
+                </div>
+                <div className="lines-of-code">
+                    <img src={img3} alt="linesofcode" />
+                    <div className="lines-number">
+                        <h2>229</h2>
+                        <p>FINISHED PROJECTS</p>
+                    </div>
+                </div>
+                <div className="lines-of-code">
+                    <img src={img4} alt="linesofcode" />
+                    <div className="lines-number">
+                        <h2>148</h2>
+                        <p>SATISFIED CLIENTS</p>
+                    </div>
+                </div>
+            </div>
+     </section>
+     </>
+  )
+}
+
+export default Achivement
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
 import './Home.css'
 import ServiceSection from '../ServiceSection/ServiceSection'
+import Achivement from '../Achivementsection/Achievement'
+
 
 const Home = () => {
   return (
     <>
    <main className='back-img'>
     <section className='hero-section'>
-    <div className="home-container">
+      <div className="home-container">
       <h4>DELIVERS VALUE</h4>
       <h2>We Deliver Value Added Services With Unique Approach To The Business Needs.</h2>
       <p>Our team of experts in Big Data, Java Development, and other IT services delivers the best services at the right time.</p>
       </div>
    </section>
    <ServiceSection/>
+   <Achivement/>
+   {/*spacer*/}
    <div className="spacer">
-    
    </div>
    </main>
   
