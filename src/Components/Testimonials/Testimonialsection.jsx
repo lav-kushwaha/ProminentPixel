1f52471abf25eb4ce73b4058f11476300c4256d6
Lav Kushwaha <lavkumar1224@gmail.com>
2023-11-06 14:42:16 +0530

    Responsive design

---
+++ src/Components/Testimonials/Testimonialsection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Testimonialsection.css'
+
+const Testimonialsection = () => {
+  return (
+    <>
+    {/* lav  */}
+    </>
+  )
+}
+
+export default Testimonialsection
